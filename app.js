780424fbbedd7db0614ddb431d53a038c5dd8aec
singhsourabh <singhsourabh14@gmail.com>
2020-02-01 14:47:59 +0530

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+var cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -12,6 +13,7 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(cors());
 app.use(express.json());
 
 // ROUTES
